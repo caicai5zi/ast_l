425e7afffe027dc277f3acd7208e8e6b4c962581
caicai5zi <caicai5zi@163.com>
2020-11-30 18:41:37 +0800

    加入引用

---
+++ ast_l/ast_l/ast_lApp.swift
@@ -0,0 +1,17 @@
+//
+//  ast_lApp.swift
+//  ast_l
+//
+//  Created by app on 2020/11/30.
+//
+
+import SwiftUI
+
+@main
+struct ast_lApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
